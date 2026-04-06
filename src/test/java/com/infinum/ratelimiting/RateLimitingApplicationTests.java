f31ae1ba6f117b3d676a0eee8101d30ef85feb86
Mario Slatinac <mario.slatinac@infinum.com>
2026-04-06 12:13:35 +0200

    Initial commit

---
+++ src/test/java/com/infinum/ratelimiting/RateLimitingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.infinum.ratelimiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
